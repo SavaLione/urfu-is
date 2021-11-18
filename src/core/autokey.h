81fd78569f477483d9ed6c7bb554099d150ce372
SavaLione <monologuesplus@gmail.com>
2021-11-18 20:27:45 +0500

    Автоключевой шифр

--- src/core/autokey.h
+++ src/core/autokey.h
@@ -5,4 +5,36 @@
 #ifndef CORE_AUTOKEY_H
 #define CORE_AUTOKEY_H
 
+#include <string>
+
+class autokey
+{
+public:
+    autokey();
+    ~autokey();
+
+    void set_key(std::string const& key);
+
+    void encrypt();
+    void decrypt();
+
+    void set_alphabet(std::string alphabet);
+    void set_source_text(std::string source_text);
+    void set_cipher_text(std::string cipher_text);
+
+    std::string get_alphabet();
+    std::string get_source_text();
+    std::string get_cipher_text();
+    std::string get_key();
+
+    int get_power();
+private:
+    std::string _alphabet;
+    std::string _source_text;
+    std::string _cipher_text;
+
+    std::string _key;
+    std::string _key_ph;
+};
+
 #endif // CORE_AUTOKEY_H
