d2fcbf62825484ae0937b8e6ac02c8e75bdc3665
SavaLione <monologuesplus@gmail.com>
2021-11-19 00:00:03 +0500

    transposition

--- src/core/transposition.h
+++ src/core/transposition.h
@@ -5,4 +5,38 @@
 #ifndef CORE_TRANSPOSITION_H
 #define CORE_TRANSPOSITION_H
 
+#include <string>
+#include <bits/stdc++.h>
+
+class transposition
+{
+public:
+    transposition();
+    ~transposition();
+
+    void encrypt();
+    void decrypt();
+
+    void set_alphabet(std::string alphabet);
+    void set_source_text(std::string source_text);
+    void set_cipher_text(std::string cipher_text);
+    void set_key(std::string const& key);
+
+    std::string get_alphabet();
+    std::string get_source_text();
+    std::string get_cipher_text();
+    std::string get_key();
+private:
+    std::string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string _source_text;
+    std::string _cipher_text;
+
+    std::string _key;
+
+    void _set_permutation_order();
+    std::string _encrypt_message(std::string msg);
+    std::string _decrypt_message(std::string cipher);
+    std::map<int,int> _key_map;
+};
+
 #endif // CORE_TRANSPOSITION_H
